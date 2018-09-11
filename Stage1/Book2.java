7b2df79517d0fdb580f3e63355afbafe1b031a94
Your Name <you@example.com>
2018-09-11 14:28:59 -0400

    Libray system

---
+++ Stage1/Book2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Book2 extends Book {
+
+
+  public Book2(String title, String author, String id) {
+    super (title, author, id);
+  }
+
+  public String getAuthor() {
+    return ("xxx");
+  }
+}
